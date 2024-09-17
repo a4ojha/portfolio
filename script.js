140c5890323a8d55ea900d6e81bb1dcfa0e5b23d
Adon Ojha <a4ojha@uwaterloo.ca>
2024-09-17 01:23:26 -0400

    fix animation skip

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementsByClassName("socials")[0].style.animationDelay = "0s";
+document.getElementsByClassName("socials")[0].style.animationDuration = "0s";
+
+document.getElementsByClassName("navbar")[0].style.animationDelay = "0s";
+document.getElementsByClassName("navbar")[0].style.animationDuration = "0s";
